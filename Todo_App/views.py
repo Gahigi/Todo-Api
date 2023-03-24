d4bd335cb0c5b319086c5f75cfe590f903be4fd6
Gahigi <jeni02893@gmail.com>
2023-03-24 09:19:04 +0200

    Todo list Api

---
+++ Todo_App/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
+
+# Create your views here.
+class ViewAllTodo(APIView):
+    def get(self, request):
+        todo = Todo.objects.all()
+        serializers_data = TodoSerializer(todo, many= True)
+        return Response(serializers_data.data, status= status.HTTP_200_OK)
+    def put(self, request):
+        serializers_data = TodoSerializer(data= data.request)
+        if serializers_data.is_valid():
+            serializers_data.save()
+            return Response(serializers_data.data, status= status.HTTP_201_CREATED)
+        return Response(serializers_data.errors, status= status.HTTP_400_BAD_REQUEST)
+class ViewTodo(APIView):
+    def get(self, request, id):
+        todo = Todo.objects.get(id = id)
+        serializers_data = TodoSerializer(todo)
+        return Response(serializers_data.data, status= status.HTTP_200_OK)
+    def post(self, request, id):
+        todo = Todo.objects.get(id = id)
+        serializers_data = TodoSerializer()    
